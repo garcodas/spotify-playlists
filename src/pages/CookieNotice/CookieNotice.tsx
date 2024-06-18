b6ffcbce0b4fabb66a7d31dd5be8ed6cab23cd81
Emanuel Garcia <emanuelgarciaaleli@hotmail.com>
2024-06-18 00:23:37 -0600

    featur(CookieNotice): Cookies Message

--- src/pages/CookieNotice/CookieNotice.tsx
+++ src/pages/CookieNotice/CookieNotice.tsx
@@ -40,13 +40,13 @@ const CookieNotice = () => {
           onClick={handleAcceptAll}
           className="bg-green-600 hover:bg-green-700 text-white px-4 py-2 rounded"
         >
-          Aceptar todas
+          Aceptar todo
         </button>
         <button
           onClick={handleRejectAll}
           className="bg-red-600 hover:bg-red-700 text-white px-4 py-2 rounded"
         >
-          Rechazar todas
+          Rechazar todo
         </button>
         <button
           onClick={handleHideNotice}
